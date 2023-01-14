1dcc385c60a3e1239e6eac310fa1d1e31c133fb2
Agatha Saez <asaez@rayensalud.com>
2023-01-14 14:22:17 -0300

    inicio de proyecto final curso c#

---
+++ Contacto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinal
+{
+    class Contacto
+    {
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }   
+
+        public Contacto() { }
+
+        public Contacto(string nombre, string telefono, string email)
+        {
+            Nombre = nombre;
+            Telefono = telefono;
+            Email = email;
+        }
+
+        string[] contactos = { };
+
+        #region metodos
+        public void AgregarContacto()
+        {
+            string[] contactos = new string[] { Nombre, Telefono, Email };
+            Console.WriteLine("El contacto\nNombre: {0}\nTeléfono: {1}\nEmail: {2}",Nombre,Telefono,Email);
+            if (contactos.Length > 0)
+            {
+                Console.WriteLine("fue agregado con éxito.");
+            } else
+            {
+                Console.WriteLine("no pudo ser agregado.");
+            }
+        }
+
+        public void VerContacto()
+        {
+            if (contactos.Length > 0)
+            {
+                foreach (string i in contactos)
+                {
+                    Console.WriteLine(i);
+                };
+            } else {
+                Console.WriteLine("No existen contactos aún.");
+            }
+
+        }
+
+        public void BorrarUltimoContacto()
+        {
+            
+            Console.WriteLine("Borrar contacto funciona");
+        }
+
+        public void Buscarcontacto()
+        {
+            Console.WriteLine("Buscar contacto funciona");
+        }
+
+        public void orden()
+        {
+            Console.WriteLine("orden funciona");
+            // Array.Sort(Contactos);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}{1}{2}", Nombre, Telefono, Email);
+        }
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ProyectoFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contacto c1 = new Contacto("Agatha", "555322461", "agatha.saez@outlook.cl");
+            Console.WriteLine(c1);
+
+            Console.WriteLine("Menú");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            #region
+            if (opcion == 1)
+            {
+                c1.VerContacto();
+            } else if (opcion == 2)
+            {
+                c1.AgregarContacto();
+            }
+            else if (opcion == 3)
+            {
+                c1.BorrarUltimoContacto();
+            }
+            else if (opcion == 4)
+            {
+                c1.Buscarcontacto();
+            } else if (opcion == 5)
+            {
+
+            } else
+            {
+                Console.WriteLine("La opción ingresada no está en el menú");
+            }
+            #endregion
+        }
+    }
+}
