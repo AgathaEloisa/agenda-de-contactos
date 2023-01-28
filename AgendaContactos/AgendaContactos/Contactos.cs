2f797ce403e4adf15565922a31fc9071c50fc107
Agatha Eloísa <ebrumaktub@gmx.com>
2023-01-28 06:53:07 -0300

    solución de warnings

---
+++ AgendaContactos/AgendaContactos/Contactos.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaContactos
+{
+    public class Contactos
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public Contactos() { 
+        }
+
+        public Contactos(string nombre, string telefono, string email)
+        {
+            Nombre = nombre;
+            Telefono = telefono;
+            Email = email;
+        }
+
+        List<Contactos> contactos = new List<Contactos>(20);
+
+        #region metodos
+        public void AgregarContacto(string nombre, string telefono, string email)
+        {
+            contactos.Add(new Contactos { Nombre = nombre, Telefono = telefono, Email = email });
+            Console.WriteLine("\nEl contacto");
+
+            if (contactos.Count > 0)
+            {
+                foreach (Contactos contacto in contactos)
+                {
+                    Console.WriteLine("\nNombre: {0}\nTeléfono: {1}\nEmail: {2}\nfue agregado con éxito.", contacto.Nombre, contacto.Telefono, contacto.Email);
+                }
+                Console.WriteLine("Contacto agregado con éxito.");
+            }
+            else
+            {
+                Console.WriteLine("no pudo ser agregado.");
+            }
+        }
+
+        public void VerContacto(int orden)
+        {
+            List<Contactos> contactosNew = new List<Contactos>(contactos);
+
+            if (contactosNew.Count > 0)
+            {
+                contactosNew.Sort(delegate (Contactos x, Contactos y) {
+                    if (x.Nombre == null && y.Nombre == null) return 0;
+                    else if (x.Nombre == null) return -1;
+                    else if (y.Nombre == null) return 1;
+                    else return x.Nombre.CompareTo(y.Nombre);
+                });
+
+                if (orden == 1)
+                {
+                    Console.WriteLine("*** Orden ascendente ***");
+
+                    foreach (Contactos contacto in contactosNew)
+                    {
+                        Console.WriteLine("\nNombre: {0}\nTeléfono: {1}\nEmail: {2}", contacto.Nombre, contacto.Telefono, contacto.Email);
+                    };
+                }
+                else if (orden == 2)
+                {
+                    Console.WriteLine("*** Orden descendente ***");
+                    contactosNew.Reverse();
+                    for (int i = 0; i < contactosNew.Count; i++)
+                    {
+                        Console.WriteLine("\nNombre: {0}\nTeléfono: {1}\nEmail: {2}", contactosNew[i].Nombre, contactosNew[i].Telefono, contactosNew[i].Email);
+                    };
+                }
+            }
+            else
+            {
+                Console.WriteLine("No existen contactos aún.");
+            }
+
+        }
+
+        public void BorrarUltimoContacto()
+        {
+            contactos.Remove(contactos[contactos.Count - 1]);
+        }
+
+        public void Buscarcontacto(string nombre)
+        {
+            Console.WriteLine(contactos[0].Nombre.Contains(nombre));
+            foreach (Contactos i in contactos)
+            {
+                if ((contactos[0].Nombre).ToLower() == nombre.ToLower())
+                {
+                    Console.WriteLine("\nNombre: {0}\nTeléfono: {1}\nEmail: {2}", i.Nombre, i.Telefono, i.Email);
+                }
+            };
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}{1}{2}", Nombre, Telefono, Email);
+        }
+
+        #endregion
+    }
+}
---
+++ AgendaContactos/AgendaContactos/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace AgendaContactos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Contactos c1 = new Contactos();
+            bool volver = true;
+            //string volverMensaje = "¿Desea volver al menú? (S/N)";
+
+            //while (volver == true)
+            //{
+            //    Console.WriteLine(volverMensaje);
+            //}
+
+
+
+            string[] opciones = { "Ver contactos", "agregar contacto", "Borrar último contacto", "Buscar contacto por nombre", "Salir" };
+            string[] opciones2 = { "Orden ascendente", "Orden descendente" };
+
+            Console.WriteLine("*** Menú ***\n");
+            do
+            {
+                for (int i = 0; i < opciones.Length; i++)
+                {
+                    Console.WriteLine("\n{0}: {1}", i + 1, opciones[i]);
+                }
+                int opcion = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    #region Menu
+                    if (opcion == 1)
+                    {
+                        bool PreguntarDenuevo = false;
+                        do
+                        {
+                            for (int i = 0; i < opciones2.Length; i++)
+                            {
+                                Console.WriteLine("\n{0}: {1}", i + 1, opciones2[i]);
+                            }
+                            int orden = Convert.ToInt32(Console.ReadLine());
+
+                            if (orden == 1 || orden == 2)
+                            {
+                                c1.VerContacto(orden);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Seleccione una opción");
+                                PreguntarDenuevo = true;
+                            }
+                        } while (PreguntarDenuevo == true);
+                    }
+                    else if (opcion == 2)
+                    {
+                        Console.Write("Nombre: ");
+                        string NuevoNombre = Console.ReadLine()!;
+                        Console.Write("\nTeléfono: ");
+                        string NuevoTelefono = Console.ReadLine()!;
+                        Console.Write("\nEmail: ");
+                        string NuevoEmail = Console.ReadLine()!;
+                        c1.AgregarContacto(NuevoNombre, NuevoTelefono, NuevoEmail);
+                    }
+                    else if (opcion == 3)
+                    {
+                        c1.BorrarUltimoContacto();
+                    }
+                    else if (opcion == 4)
+                    {
+                        Console.WriteLine("Ingrese nombre del contacto");
+                        string nombre = Console.ReadLine()!;
+                        c1.Buscarcontacto(nombre);
+                    }
+                    else if (opcion == 5)
+                    {
+                        volver = false;
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("La opción ingresada no está en el menú");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ha ocurrido un error");
+                }
+
+            }
+            while (volver == true);
+            #endregion
+        }
+    }
+}
